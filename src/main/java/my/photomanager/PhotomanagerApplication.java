7e097e3a959f0d71e365c90d696e2437a1d89d82
Sebastian Büge <74074904+sbuege@users.noreply.github.com>
2025-10-06 11:10:47 +0200

    add project files (#10)
    add gitignore file
    add gradle project files

---
+++ src/main/java/my/photomanager/PhotomanagerApplication.java
@@ -0,0 +1,13 @@
+package my.photomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotomanagerApplication.class, args);
+	}
+
+}
