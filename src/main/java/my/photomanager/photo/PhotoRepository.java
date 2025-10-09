bafc8e45b71e826ee35e4b3d696b20aae87054bc
Sebastian Büge <sebastian_buege@gmx.de>
2025-10-09 09:53:28 +0200

    add photo repository with tests

---
+++ src/main/java/my/photomanager/photo/PhotoRepository.java
@@ -0,0 +1,10 @@
+package my.photomanager.photo;
+
+import java.util.Optional;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+
+	Optional<Photo> findByHashValue(@NonNull String hashValue);
+}
