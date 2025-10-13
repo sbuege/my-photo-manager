ff9cd46656f2631750e69a2652a4436fe996cfd3
Sebastian Büge <sebastian_buege@gmx.de>
2025-10-13 09:26:46 +0200

    use long as id in repository

--- src/main/java/my/photomanager/photo/album/PhotoAlbumRepository.java
+++ src/main/java/my/photomanager/photo/album/PhotoAlbumRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhotoAlbumRepository extends JpaRepository<PhotoAlbum, Integer> {
+public interface PhotoAlbumRepository extends JpaRepository<PhotoAlbum, Long> {
 
 	Optional<PhotoAlbum> findByName(@NonNull String name);
 }
