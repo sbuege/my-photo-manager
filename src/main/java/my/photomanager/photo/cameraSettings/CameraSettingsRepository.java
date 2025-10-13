3e9de52357ef028a062aa786a338c0d21a685b67
Sebastian Büge <sebastian_buege@gmx.de>
2025-10-13 09:26:12 +0200

    use long as id in reposiotries

--- src/main/java/my/photomanager/photo/cameraSettings/CameraSettingsRepository.java
+++ src/main/java/my/photomanager/photo/cameraSettings/CameraSettingsRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CameraSettingsRepository extends JpaRepository<CameraSettings, Integer> {
+public interface CameraSettingsRepository extends JpaRepository<CameraSettings, Long> {
 
 	Optional<CameraSettings> findByCameraModelName(@NonNull String cameraModelName);
 }
