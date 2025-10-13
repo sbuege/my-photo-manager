bc1dba9bf9e5d9921988e2704683f590ae83a89e
Sebastian Büge <sebastian_buege@gmx.de>
2025-10-13 09:27:10 +0200

    add filter logic in service

--- src/main/java/my/photomanager/photo/PhotoService.java
+++ src/main/java/my/photomanager/photo/PhotoService.java
@@ -3,8 +3,12 @@ package my.photomanager.photo;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
@@ -38,4 +42,38 @@ public class PhotoService {
 					return savedPhoto;
 				});
 	}
+
+	public Collection<Photo> filterPhotos(LocalDate start, LocalDate end, List<Long> locationIds) {
+		return photoRepository.findAll(Specification.where(containsCreationDateBetween(start, end))
+				.and(containsLocation(locationIds)));
+	}
+
+	private static Specification<Photo> containsCreationDateBetween(LocalDate start, LocalDate end) {
+		return (root, query, cb) -> {
+			if (start == null && end == null) {
+				return null;
+			}
+			if (start != null && end != null) {
+				return cb.between(root.get("creationDate"), start, end);
+			}
+			if (start != null) {
+				return cb.greaterThanOrEqualTo(root.get("creationDate"), start);
+			}
+			return cb.lessThanOrEqualTo(root.get("creationDate"), end);
+		};
+	}
+
+	private static Specification<Photo> containsLocation(List<Long> locationIds) {
+		return (root, query, cb) -> {
+			if (locationIds == null || locationIds.isEmpty()) {
+				return null;
+			}
+			return root.get("location")
+					.get("id")
+					.in(locationIds);
+
+		};
+
+	}
+
 }
