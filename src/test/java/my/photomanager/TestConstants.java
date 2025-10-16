5ed1ce1d74072b0763174371eca063f673d1d2b0
Sebastian Büge <sebastian_buege@gmx.de>
2025-10-16 14:16:17 +0200

    update test constants

--- src/test/java/my/photomanager/TestConstants.java
+++ src/test/java/my/photomanager/TestConstants.java
@@ -1,26 +1,54 @@
 package my.photomanager;
 
 import java.nio.file.Path;
-import java.util.stream.Stream;
+import java.time.LocalDate;
+import org.apache.logging.log4j.util.Strings;
 
 public class TestConstants {
 
 	public static Path TestFilePath = Path.of("src", "test", "resources", "Testdata");
 
-	public static Stream<Path> jpegWithExifDataFileProvider() {
-		return Stream.of(TestFilePath.resolve(Path.of("Berlin_WithExifData.jpg")));
-	}
+	// example 001
+	public static Path EXAMPLE_001_PATH = TestFilePath.resolve("example_001.jpg");
+	public static int EXAMPLE_001_HEIGHT = 750;
+	public static int EXAMPLE_001_WIDTH = 1000;
+	public static LocalDate EXAMPLE_001_CREATION_DATE = LocalDate.of(2024, 1, 1);
+	public static String EXAMPLE_001_CAMERA_MODEL = "iPhone 14 Pro";
+	public static double EXAMPLE_001_LONGITUDE = -80.128525;
+	public static double EXAMPLE_001_LATITUDE = 25.7862;
+	public static String EXAMPLE_001_COUNTRY = "Vereinigte Staaten von Amerika";
+	public static String EXAMPLE_001_CITY = "Miami Beach";
 
-	public static Stream<Path> jpegWithoutExifDataFileProvider() {
-		return Stream.of(TestFilePath.resolve(Path.of("Berlin_WithoutExifData.jpg")));
-	}
+	// example 002
+	public static Path EXAMPLE_002_PATH = TestFilePath.resolve("example_002.jpg");
+	public static int EXAMPLE_002_HEIGHT = 693;
+	public static int EXAMPLE_002_WIDTH = 1000;
+	public static LocalDate EXAMPLE_002_CREATION_DATE = LocalDate.of(2024, 1, 1);
+	public static String EXAMPLE_002_CAMERA_MODEL = "Canon EOS-1D Mark IV";
+	public static double EXAMPLE_002_LONGITUDE = 13.376194444444446;
+	public static double EXAMPLE_002_LATITUDE = 52.518680555555555;
+	public static String EXAMPLE_002_COUNTRY = "Deutschland";
+	public static String EXAMPLE_002_CITY = "Berlin";
 
-	public static Stream<Path> webWithExifDataFileProvider() {
-		return Stream.of(TestFilePath.resolve(Path.of("Berlin_WithExifData.webp")));
-	}
+	// example 003
+	public static Path EXAMPLE_003_PATH = TestFilePath.resolve("example_003.jpg");
+	public static int EXAMPLE_003_HEIGHT = 750;
+	public static int EXAMPLE_003_WIDTH = 1000;
+	public static LocalDate EXAMPLE_003_CREATION_DATE = LocalDate.of(2025, 1, 1);
+	public static String EXAMPLE_003_CAMERA_MODEL = "iPhone 14 Pro";
+	public static double EXAMPLE_003_LONGITUDE = -80.128525;
+	public static double EXAMPLE_003_LATITUDE = 25.7862;
+	public static String EXAMPLE_003_COUNTRY = "Vereinigte Staaten von Amerika";
+	public static String EXAMPLE_003_CITY = "Miami Beach";
 
-	public static Stream<Path> webWithoutExifDataFileProvider() {
-		return Stream.of(TestFilePath.resolve(Path.of("Berlin_WithoutExifData.webp")));
-	}
-
+	// example 004
+	public static Path EXAMPLE_004_PATH = TestFilePath.resolve("example_004.webp");
+	public static int EXAMPLE_004_HEIGHT = 768;
+	public static int EXAMPLE_004_WIDTH = 1024;
+	public static LocalDate EXAMPLE_004_CREATION_DATE = LocalDate.of(2025, 1, 1);
+	public static String EXAMPLE_004_CAMERA_MODEL = Strings.EMPTY;
+	public static double EXAMPLE_004_LONGITUDE = 13.376194444444446;
+	public static double EXAMPLE_004_LATITUDE = 52.518680555555555;
+	public static String EXAMPLE_004_COUNTRY = "Deutschland";
+	public static String EXAMPLE_004_CITY = "Berlin";
 }
