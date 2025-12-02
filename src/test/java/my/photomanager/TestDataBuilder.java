7d1b826fb63aa3d6acb91bb6f939ab7432ccecf6
Sebastian Büge <sebastian_buege@gmx.de>
2025-12-02 10:27:05 +0100

    update test data

--- src/test/java/my/photomanager/TestDataBuilder.java
+++ src/test/java/my/photomanager/TestDataBuilder.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Optional;
 import my.photomanager.metadata.Metadata;
+import my.photomanager.photo.Orientation;
 import my.photomanager.photo.Photo;
 import my.photomanager.photo.album.Album;
 import my.photomanager.photo.cameraModel.CameraModel;
@@ -18,6 +19,7 @@ public class TestDataBuilder {
 	public static Path EXAMPLE_001_PATH = TestFilePath.resolve("example_001.jpg");
 	public static Integer EXAMPLE_001_HEIGHT = 750;
 	public static Integer EXAMPLE_001_WIDTH = 1000;
+	public static Orientation EXAMPLE_001_ORIENTATION = Orientation.LANDSCAPE;
 	public static LocalDate EXAMPLE_001_CREATION_DATE = LocalDate.of(2024, 1, 1);
 	public static String EXAMPLE_001_CAMERA_MODEL = "iPhone 14 Pro";
 	public static Double EXAMPLE_001_LONGITUDE = -80.128525;
@@ -30,6 +32,7 @@ public class TestDataBuilder {
 					.getAbsolutePath())
 			.withWidth(EXAMPLE_001_WIDTH)
 			.withHeight(EXAMPLE_001_HEIGHT)
+			.withOrientation(EXAMPLE_001_ORIENTATION)
 			.withCreationDate(EXAMPLE_001_CREATION_DATE)
 			.withCameraModel(TestCameraModelBuilder.build(EXAMPLE_001_CAMERA_MODEL))
 			.withLocation(TestPhotoLocationBuilder.build(EXAMPLE_001_COUNTRY, EXAMPLE_001_CITY))
@@ -39,6 +42,7 @@ public class TestDataBuilder {
 	public static Path EXAMPLE_002_PATH = TestFilePath.resolve("example_002.jpg");
 	public static Integer EXAMPLE_002_HEIGHT = 693;
 	public static Integer EXAMPLE_002_WIDTH = 1000;
+	public static Orientation EXAMPLE_002_ORIENTATION = Orientation.LANDSCAPE;
 	public static LocalDate EXAMPLE_002_CREATION_DATE = LocalDate.of(2024, 1, 1);
 	public static String EXAMPLE_002_CAMERA_MODEL = "Canon EOS-1D Mark IV";
 	public static Double EXAMPLE_002_LONGITUDE = 13.376194444444446;
@@ -47,9 +51,11 @@ public class TestDataBuilder {
 	public static String EXAMPLE_002_CITY = "Berlin";
 	public static Photo EXAMPLE_002_PHOTO = Photo.builder()
 			.withHashValue("123456789")
-			.withFileName(EXAMPLE_002_PATH.toFile().getAbsolutePath())
+			.withFileName(EXAMPLE_002_PATH.toFile()
+					.getAbsolutePath())
 			.withWidth(EXAMPLE_002_WIDTH)
 			.withHeight(EXAMPLE_002_HEIGHT)
+			.withOrientation(EXAMPLE_002_ORIENTATION)
 			.withCreationDate(EXAMPLE_002_CREATION_DATE)
 			.withCameraModel(TestCameraModelBuilder.build(EXAMPLE_002_CAMERA_MODEL))
 			.withLocation(TestPhotoLocationBuilder.build(EXAMPLE_002_COUNTRY, EXAMPLE_002_CITY))
@@ -59,6 +65,7 @@ public class TestDataBuilder {
 	public static Path EXAMPLE_003_PATH = TestFilePath.resolve("example_003.jpg");
 	public static Integer EXAMPLE_003_HEIGHT = 750;
 	public static Integer EXAMPLE_003_WIDTH = 1000;
+	public static Orientation EXAMPLE_003_ORIENTATION = Orientation.LANDSCAPE;
 	public static LocalDate EXAMPLE_003_CREATION_DATE = LocalDate.of(2025, 1, 1);
 	public static String EXAMPLE_003_CAMERA_MODEL = "iPhone 14 Pro";
 	public static Double EXAMPLE_003_LONGITUDE = -80.128525;
@@ -67,9 +74,11 @@ public class TestDataBuilder {
 	public static String EXAMPLE_003_CITY = "Miami Beach";
 	public static Photo EXAMPLE_003_PHOTO = Photo.builder()
 			.withHashValue("123456789")
-			.withFileName(EXAMPLE_003_PATH.toFile().getAbsolutePath())
+			.withFileName(EXAMPLE_003_PATH.toFile()
+					.getAbsolutePath())
 			.withWidth(EXAMPLE_003_WIDTH)
 			.withHeight(EXAMPLE_003_HEIGHT)
+			.withOrientation(EXAMPLE_003_ORIENTATION)
 			.withCreationDate(EXAMPLE_003_CREATION_DATE)
 			.withCameraModel(TestCameraModelBuilder.build(EXAMPLE_003_CAMERA_MODEL))
 			.withLocation(TestPhotoLocationBuilder.build(EXAMPLE_003_COUNTRY, EXAMPLE_003_CITY))
@@ -79,6 +88,7 @@ public class TestDataBuilder {
 	public static Path EXAMPLE_004_PATH = TestFilePath.resolve("example_004.webp");
 	public static Integer EXAMPLE_004_HEIGHT = 768;
 	public static Integer EXAMPLE_004_WIDTH = 1024;
+	public static Orientation EXAMPLE_004_ORIENTATION = Orientation.LANDSCAPE;
 	public static LocalDate EXAMPLE_004_CREATION_DATE = LocalDate.of(2025, 1, 1);
 	public static String EXAMPLE_004_CAMERA_MODEL = "iPhone 14 Pro";
 	public static Double EXAMPLE_004_LONGITUDE = 13.376194444444446;
@@ -87,9 +97,11 @@ public class TestDataBuilder {
 	public static String EXAMPLE_004_CITY = "Berlin";
 	public static Photo EXAMPLE_004_PHOTO = Photo.builder()
 			.withHashValue("123456789")
-			.withFileName(EXAMPLE_004_PATH.toFile().getAbsolutePath())
+			.withFileName(EXAMPLE_004_PATH.toFile()
+					.getAbsolutePath())
 			.withWidth(EXAMPLE_004_WIDTH)
 			.withHeight(EXAMPLE_004_HEIGHT)
+			.withOrientation(EXAMPLE_004_ORIENTATION)
 			.withCreationDate(EXAMPLE_004_CREATION_DATE)
 			.withCameraModel(TestCameraModelBuilder.build(EXAMPLE_004_CAMERA_MODEL))
 			.withLocation(TestPhotoLocationBuilder.build(EXAMPLE_004_COUNTRY, EXAMPLE_004_CITY))
@@ -104,6 +116,7 @@ public class TestDataBuilder {
 	public static final String TEST_PHOTO_FILE_NAME = "TestPhotoFileName";
 	public static final Integer TEST_PHOTO_WIDTH = 1000;
 	public static final Integer TEST_PHOTO_HEIGHT = 750;
+	public static final Orientation TEST_PHOTO_ORIENTATION = Orientation.LANDSCAPE;
 	public static final LocalDate TEST_PHOTO_CREATION_DATE = LocalDate.of(2024, 1, 1);
 	public static final Double TEST_PHOTO_LONGITUDE = -80.128525;
 	public static final Double TEST_PHOTO_LATITUDE = 25.7862;
@@ -164,16 +177,17 @@ public class TestDataBuilder {
 	public static class TestPhotoBuilder {
 
 		public static Photo build() {
-			return build(TEST_PHOTO_HASH_VALUE, TEST_PHOTO_FILE_NAME, TEST_PHOTO_HEIGHT, TEST_PHOTO_WIDTH, TEST_PHOTO_CREATION_DATE);
+			return build(TEST_PHOTO_HASH_VALUE, TEST_PHOTO_FILE_NAME, TEST_PHOTO_HEIGHT, TEST_PHOTO_WIDTH, TEST_PHOTO_ORIENTATION, TEST_PHOTO_CREATION_DATE);
 		}
 
-		public static Photo build(String hashValue, String fileName, int height, int width, LocalDate creationDate) {
+		public static Photo build(String hashValue, String fileName, int height, int width, Orientation orientation, LocalDate creationDate) {
 			return Photo.builder()
 					.withHashValue(hashValue)
 					.withFileName(fileName)
-					.withWidth(TEST_PHOTO_WIDTH)
-					.withHeight(TEST_PHOTO_HEIGHT)
-					.withCreationDate(TEST_PHOTO_CREATION_DATE)
+					.withWidth(width)
+					.withHeight(height)
+					.withOrientation(orientation)
+					.withCreationDate(creationDate)
 					.build();
 		}
 	}
