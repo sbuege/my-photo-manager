7dd2f158f0ce8bcb575a8632354d21cbeccaca8c
Sebastian Büge <sebastian_buege@gmx.de>
2025-12-02 10:07:17 +0100

    filter by orientation

--- src/main/java/my/photomanager/photo/Photo.java
+++ src/main/java/my/photomanager/photo/Photo.java
@@ -2,6 +2,8 @@ package my.photomanager.photo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -48,6 +50,11 @@ public class Photo {
 	@NonNull
 	@Column(updatable = false)
 	private Integer width;
+
+	@NonNull
+	@Column(updatable = false)
+	@Enumerated(EnumType.STRING)
+	private Orientation orientation;
 
 	@Column(name = "creationDate", columnDefinition = "DATE")
 	private LocalDate creationDate;
