f659151bdc468587564f55591958de622a1f0810
Sebastian Büge <sebastian_buege@gmx.de>
2025-12-05 13:47:31 +0100

    add business logic for library service

--- src/main/java/my/photomanager/library/Library.java
+++ src/main/java/my/photomanager/library/Library.java
@@ -1,5 +1,40 @@
 package my.photomanager.library;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Builder(setterPrefix = "with", toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+@Entity
+@Table(name = "library")
+@ToString
 public class Library {
+
+	@Id
+	@GeneratedValue
+	@Column(updatable = false, unique = true)
+	private long id;
+
+	@Column(unique = true, nullable = false)
+	@NonNull
+	private String name;
+
+	@Column(unique = true, nullable = false)
+	@NonNull
+	String path;
 
 }
--- src/main/java/my/photomanager/library/LibrayService.java
+++ src/main/java/my/photomanager/library/LibrayService.java
@@ -1,5 +1,77 @@
 package my.photomanager.library;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import my.photomanager.photo.PhotoBuilder;
+import my.photomanager.photo.PhotoBuilderException;
+import my.photomanager.photo.PhotoService;
+import my.photomanager.utils.gpsResolver.GpsResolverException;
+import my.photomanager.utils.metaDataParser.MetadataParserException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Log4j2
 public class LibrayService {
+
+	private final List<String> PHOTO_EXTENSIONS = List.of("jpg", "jpeg", "webp");
+
+	private final LibraryRepository repository;
+	private final PhotoBuilder photoBuilder;
+	private final PhotoService photoService;
+
+	/**
+	 * Saves the given {@link Library} if no library with the same path exists
+	 * or update the existing one.
+	 *
+	 * @param library the {@link Library} to save or retrieve
+	 */
+	public void saveLibray(@NonNull Library library) {
+		repository.findByPath(library.getPath())
+				.orElseGet(() -> {
+					var savedPhotoAlum = repository.saveAndFlush(library);
+					log.info("saved library with path {} successfully", library.getPath());
+
+					return savedPhotoAlum;
+				});
+	}
+
+	public void indexLibrary(@NonNull Library library) {
+
+		log.info("indexing photos from {}", library.getPath());
+
+		try {
+			Files.walk(Path.of(library.getPath()))
+					.filter(Files::isRegularFile)
+					.filter(this::isPhotoFile)
+					.forEach((path) -> {
+						try {
+							if (!photoService.existsPhotoByHashValue(path)) {
+								var photo = photoBuilder.buildPhoto(path);
+								photoService.saveIfPhotoNotExists(photo);
+							}
+						} catch (MetadataParserException | GpsResolverException | IOException | PhotoBuilderException e) {
+							throw new RuntimeException(e);
+						}
+					});
+		} catch (IOException e) {
+			log.error("an error occurred while indexing photos", e);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private boolean isPhotoFile(@NonNull Path path) {
+		String fileName = path.getFileName()
+				.toString()
+				.toLowerCase();
+		return PHOTO_EXTENSIONS.stream()
+				.anyMatch(fileName::endsWith);
+	}
 
 }
