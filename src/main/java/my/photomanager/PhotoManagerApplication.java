83f45397b9f06e70da4c7644b3fa200c6dc055e6
Sebastian Büge <sebastian_buege@gmx.de>
2025-12-05 13:48:31 +0100

    remove indexer from application

--- src/main/java/my/photomanager/PhotoManagerApplication.java
+++ src/main/java/my/photomanager/PhotoManagerApplication.java
@@ -1,26 +1,12 @@
 package my.photomanager;
 
-import my.photomanager.indexer.Indexer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class PhotoManagerApplication implements CommandLineRunner {
-
-	private final Indexer photoIndexer;
-
-	protected PhotoManagerApplication(Indexer photoIndexer) {
-		this.photoIndexer = photoIndexer;
-	}
-
-
-	@Override
-	public void run(String... args) {
-		photoIndexer.indexPhotos();
-	}
+public class PhotoManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoManagerApplication.class, args);
