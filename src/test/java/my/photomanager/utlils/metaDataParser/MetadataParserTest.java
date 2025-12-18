8db84d4e0db0a9dfb08f3264581b3968e1a2b316
Sebastian Büge <sebastian_buege@gmx.de>
2025-12-18 14:34:40 +0100

    move test classes

--- src/test/java/my/photomanager/utlils/metaDataParser/MetadataParserTest.java
+++ src/test/java/my/photomanager/utlils/metaDataParser/MetadataParserTest.java
@@ -1,4 +1,4 @@
-package my.photomanager.metadataparser;
+package my.photomanager.utlils.metaDataParser;
 
 import static my.photomanager.TestDataBuilder.EXAMPLE_001_CAMERA_MODEL;
 import static my.photomanager.TestDataBuilder.EXAMPLE_001_CREATION_DATE;
