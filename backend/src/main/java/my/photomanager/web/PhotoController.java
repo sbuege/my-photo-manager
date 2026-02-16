417630c22a688a4abfa9612b1752dd0faea6d652
Sebastian Büge <sebastian_buege@gmx.de>
2026-02-16 16:09:32 +0100

    controller return PhotoResponse

--- backend/src/main/java/my/photomanager/web/PhotoController.java
+++ backend/src/main/java/my/photomanager/web/PhotoController.java
@@ -6,14 +6,21 @@ import java.nio.file.Path;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import my.photomanager.core.cameraModel.CameraModelStatistic;
+import my.photomanager.core.photo.Photo;
 import my.photomanager.core.photo.PhotoService;
 import my.photomanager.core.photo.PhotoServiceException;
 import my.photomanager.web.filter.ActiveFilter;
 import my.photomanager.web.filter.FilterService;
+import my.photomanager.web.response.CameraModelResponse;
+import my.photomanager.web.response.PhotoResponse;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,43 +36,45 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class PhotoController {
 
-	private final PhotoService photoService;
-	private final FilterService filterService;
+    private final PhotoService photoService;
+    private final FilterService filterService;
 
 
-	@ExceptionHandler(PhotoServiceException.class)
-	public ResponseEntity<String> handlePhotoServiceExceptionException(PhotoServiceException e) {
-		return ResponseEntity.badRequest()
-				.body(e.getMessage());
-	}
+    @ExceptionHandler(PhotoServiceException.class)
+    public ResponseEntity<String> handlePhotoServiceExceptionException(PhotoServiceException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
 
-	@GetMapping("/")
-	protected ResponseEntity<List<Long>> getPhotos() {
-		var emptyActiveFilter = new ActiveFilter(List.of(), List.of(), List.of(), List.of());
-		var photoIDs = filterService.filterPhotos(emptyActiveFilter);
+    @GetMapping("/")
+    protected ResponseEntity<List<PhotoResponse>> getPhotos() {
+        Function<Photo, PhotoResponse> map2PhotoResponse = (photo) -> new PhotoResponse(photo.getId(), photo.getCameraModel().getName());
 
-		return ResponseEntity.ok(photoIDs);
-	}
+        var emptyActiveFilter = new ActiveFilter(List.of(), List.of(), List.of(), List.of());
 
-	@GetMapping("/thumbnail/{ID}")
-	protected ResponseEntity<byte[]> getPhotoThumbnail(@PathVariable long ID) throws IOException {
-		return ResponseEntity.ok()
-				.contentType(MediaType.IMAGE_JPEG)
-				.body(getThumbnail(ID));
-	}
 
-	private byte[] getThumbnail(@NonNull Long ID) throws IOException {
-		var photo = photoService.findById(ID);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
+        return ResponseEntity.ok(filterService.filterPhotos(emptyActiveFilter).stream().map(map2PhotoResponse).collect(Collectors.toList()));
+    }
 
-		Thumbnails.of(Path.of(photo.getFileName())
-						.toFile())
-				.scale(0.25)
-				.toOutputStream(out);
+    @GetMapping("/thumbnail/{ID}")
+    protected ResponseEntity<byte[]> getPhotoThumbnail(@PathVariable long ID) throws IOException {
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(getThumbnail(ID));
+    }
 
-		Base64.getEncoder()
-				.encodeToString(out.toByteArray());
+    private byte[] getThumbnail(@NonNull Long ID) throws IOException {
+        var photo = photoService.findById(ID);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-		return out.toByteArray();
-	}
+        Thumbnails.of(Path.of(photo.getFileName())
+                        .toFile())
+                .scale(1)
+                .toOutputStream(out);
+
+        Base64.getEncoder()
+                .encodeToString(out.toByteArray());
+
+        return out.toByteArray();
+    }
 }
---
+++ backend/src/main/java/my/photomanager/web/response/PhotoResponse.java
@@ -0,0 +1,4 @@
+package my.photomanager.web.response;
+
+public record PhotoResponse (long id, String cameraModel){
+}
