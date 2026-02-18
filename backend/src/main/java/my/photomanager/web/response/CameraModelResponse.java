74d693eb1a9b14166ec5dcb09749fe38f394e575
Sebastian Büge <sebastian_buege@gmx.de>
2026-02-18 15:21:06 +0100

    remove unused classes

--- backend/src/main/java/my/photomanager/web/response/CameraModelResponse.java
+++
@@ -1,5 +0,0 @@
-package my.photomanager.web.response;
-
-public record CameraModelResponse(long id, String name) {
-
-}
--- backend/src/main/java/my/photomanager/web/response/LocationResponse.java
+++
@@ -1,5 +0,0 @@
-package my.photomanager.web.response;
-
-public record LocationResponse(long id, String country, String city) {
-
-}
